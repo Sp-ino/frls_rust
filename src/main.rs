01f7c4c5d079347520a75f0716becbe57f632aed
sp-ino <spinovale97@gmail.com>
2023-01-20 18:11:15 +0100

    Improving plot_trace. Also, update .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use core::panic;
 use matfile::MatFile;
-use plotters::prelude::*;
+use plotters::{prelude::*};
 
 
 
@@ -11,28 +11,22 @@ fn plot_trace(curve: Vec<(f64, f64)>, path: &str) -> Result<(), Box<dyn std::err
 
     let mut chart = ChartBuilder::on(&root)
                 // Set the caption of the chart
-                .caption("Sample trace", ("sans-serif", 40).into_font())
+                .caption("Sample trace", ("sans-serif", 20).into_font())
                 // Set the size of the label region
                 .x_label_area_size(30)
                 .y_label_area_size(60)
                 // Finally attach a coordinate on the drawing area and make a chart context
-                .build_cartesian_2d(0.0f64..0.8e-5f64, -0.3e5f64..0.4e5f64)?;
+                .build_cartesian_2d(0.0f64..0.8e-5f64, -0.3e5f64..0.3e5f64)?;
 
     chart
         .configure_mesh()
-        .disable_x_mesh()
-        .disable_y_mesh()
+        .disable_mesh()
         .draw()?;
 
     chart
         .draw_series(LineSeries::new(curve,
                                 &BLUE))?;
 
-    chart
-        .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
-        .draw()?;
 
     root.present()?;
 
@@ -52,8 +46,8 @@ fn get_data(filename: &str) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
         print!("Found array named {:?}\n", arr.name());
     }
     let t = file.find_by_name("t").unwrap().data();
-    let idata = file.find_by_name("idata").unwrap().data();
-    let odata = file.find_by_name("odata2").unwrap().data();
+    let idata = file.find_by_name("idata").unwrap_or_else(|| panic!("idata field is None!")).data();
+    let odata = file.find_by_name("odata2").unwrap_or_else(|| panic!("odata2 field is None!")).data();
 
     let time;
     let input;
@@ -88,9 +82,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut curve = Vec::new();
 
-    let (t, input, targ) = get_data(filename);
+    let (t, _input, targ) = get_data(filename);
 
-    for (i, (px, py)) in t.iter().zip(targ).enumerate() {
+    for (_i, (px, py)) in t.iter().zip(targ).enumerate() {
         // print!("{} {}", px, py);
         let px = *px;
         let py = py;
